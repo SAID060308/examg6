a9d52d0a475a1a09d0e3f4ceb504a98c42340ab7
Said <said060308@gmail.com>
2025-04-28 17:26:17 +0500

    Proekt yaratildi

---
+++ src/main/java/com/example/examg6/ExamG6Application.java
@@ -0,0 +1,13 @@
+package com.example.examg6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamG6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamG6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examg6/ExamG6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examg6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamG6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
