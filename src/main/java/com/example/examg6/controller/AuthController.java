655553a3de46105cae4296a3b535dceaae1bec17
Said <said060308@gmail.com>
2025-04-29 18:50:28 +0500

    login togrilandi

--- src/main/java/com/example/examg6/controller/AuthController.java
+++ src/main/java/com/example/examg6/controller/AuthController.java
@@ -1,4 +1,12 @@
 package com.example.examg6.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AuthController {
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
 }
